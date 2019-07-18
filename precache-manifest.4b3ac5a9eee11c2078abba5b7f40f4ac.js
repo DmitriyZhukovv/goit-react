533e87d4658ebe8a4d5ef521b836bc04544eac03
DmitriyZhukovv <d.zhukovv@gmail.com>
2019-07-18 13:17:10 +0300

    Updates

---
+++ precache-manifest.4b3ac5a9eee11c2078abba5b7f40f4ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df97d2430b863eb3766ae60cefa6fab",
+    "url": "/goit-react/index.html"
+  },
+  {
+    "revision": "54d8973f35a48a48c1e2",
+    "url": "/goit-react/static/css/main.8f0c73fb.chunk.css"
+  },
+  {
+    "revision": "f15035f96825af289fd0",
+    "url": "/goit-react/static/js/2.978a140f.chunk.js"
+  },
+  {
+    "revision": "54d8973f35a48a48c1e2",
+    "url": "/goit-react/static/js/main.370a3c64.chunk.js"
+  },
+  {
+    "revision": "162f8e5b86febdfc1b6a",
+    "url": "/goit-react/static/js/runtime~main.3c88ed6c.js"
+  }
+]);
